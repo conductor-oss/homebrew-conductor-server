efee24a82d4c1cd0212d32f58202e7de55e1c94e
nthmost-orkes <naomi.most@orkes.io>
2026-04-23 13:55:04 -0700

    Fix all brew audit issues and Java path in wrapper script
    - desc: remove formula-name prefix per Homebrew style
    - license: Apache 2.0 → Apache-2.0 (valid SPDX identifier)
    - version: remove redundant field (inferred from URL)
    - wrapper: pin java binary to openjdk@21 opt path so the formula
      works regardless of system default Java version
    - test: assert_path_exists instead of deprecated assert_predicate :exist?

--- Formula/conductor.rb
+++ Formula/conductor.rb
@@ -1,10 +1,9 @@
 class Conductor < Formula
-  desc "Conductor OSS Server"
+  desc "Open source workflow orchestration server"
   homepage "https://github.com/conductor-oss/conductor"
   url "https://github.com/conductor-oss/conductor/releases/download/v3.23.0/conductor-lite-3.23.0.jar"
   sha256 "68025e308151ceb0c40788193ae61065d24dd8cd1e8bbbd03ae87e9c9591134f"
-  license "Apache 2.0"
-  version "3.23.0"
+  license "Apache-2.0"
 
   depends_on "openjdk@21"
 
@@ -12,13 +11,13 @@ class Conductor < Formula
     libexec.install "conductor-lite-3.23.0.jar"
     (bin/"conductor").write <<~EOS
       #!/bin/bash
-      exec java -jar #{libexec}/conductor-lite-3.23.0.jar "$@"
+      exec "#{Formula["openjdk@21"].opt_bin}/java" -jar "#{libexec}/conductor-lite-3.23.0.jar" "$@"
     EOS
     chmod 0755, bin/"conductor"
   end
 
   test do
     assert_predicate bin/"conductor", :executable?
-    assert_predicate libexec/"conductor-lite-3.23.0.jar", :exist?
+    assert_path_exists libexec/"conductor-lite-3.23.0.jar"
   end
 end
